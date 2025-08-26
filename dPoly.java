3faa6fcd609f7330e9ee000e3ada26849e01eec9
VikulChoudhary <chvikul.9999@gmail.com>
2025-08-26 10:42:30 +0530

    Add files via upload

---
+++ dPoly.java
@@ -0,0 +1,19 @@
+package Class1;
+class A1{
+    void m1(){
+        System.out.println("Coming from m1");
+    }
+}
+class B1 extends A1{
+    void m1(){
+        System.out.println("Coming from m2");
+    }
+}
+public class dPoly {
+    public static void main(String[] args) {
+        System.out.println("A");
+        A1 b1 = new B1();
+        b1.m1();
+
+    }
+}
